755ede535a5195027ef54ee290984ec32a0dd960
Henrik Joreteg <hjoreteg@gmail.com>
2013-09-11 15:57:01 -0700

    initial stab at adding data channels. part of HenrikJoreteg/SimpleWebRTC#32

--- webrtc.js
+++ webrtc.js
@@ -30,7 +30,8 @@ function WebRTC(opts) {
                 audio: true,
                 video: true
             },
-            detectSpeakingEvents: true
+            detectSpeakingEvents: true,
+            enableDataChannels: true
         };
     var item, connection;
 
@@ -242,11 +243,18 @@ function Peer(options) {
     this.sharemyscreen = options.sharemyscreen || false;
     this.browserPrefix = options.prefix;
     this.stream = options.stream;
+    this.channels = {};
     // Create an RTCPeerConnection via the polyfill
     this.pc = new PeerConnection(this.parent.config.peerConnectionConfig, this.parent.config.peerConnectionContraints);
     this.pc.on('ice', this.onIceCandidate.bind(this));
     this.pc.on('addStream', this.handleRemoteStreamAdded.bind(this));
+    this.pc.on('addChannel', this.handleDataChannelAdded.bind(this));
     this.pc.on('removeStream', this.handleStreamRemoved.bind(this));
+    // Just fire negotiation needed events for now
+    // When browser re-negotiation handling seems to work
+    // we can use this as the trigger for starting the offer/answer process
+    // automatically. We'll just leave it be for now while this stabalizes.
+    this.pc.on('negotiationNeeded', this.emit.bind(this, 'negotiationNeeded'));
     this.logger = this.parent.logger;
 
     // handle screensharing/broadcast mode
@@ -260,12 +268,22 @@ function Peer(options) {
         this.pc.addStream(this.parent.localStream);
     }
 
+    if (this.parent.config.enableDataChannels && webrtc.dataChannel) {
+        // we may not have reliable channels
+        try {
+            this.reliableChannel = this.getDataChannel('reliable', {reliable: true});
+        } catch (e) {
+            this.reliableChannel = false;
+        }
+        this.unreliableChannel = this.getDataChannel('unreliable', {reliable: false});
+    }
+
     // call emitter constructor
     WildEmitter.call(this);
 
     // proxy events to parent
-    this.on('*', function (name, value) {
-        self.parent.emit(name, value, self);
+    this.on('*', function () {
+        self.parent.emit.apply(self.parent, arguments);
     });
 }
 
@@ -310,6 +328,29 @@ Peer.prototype.send = function (messageType, payload) {
     this.parent.emit('message', message);
 };
 
+// Internal method registering handlers for a data channel and emitting events on the peer
+Peer.prototype._observeDataChannel = function (channel) {
+    var self = this;
+    channel.onclose = this.emit.bind(this, 'channelClose', channel);
+    channel.onerror = this.emit.bind(this, 'channelError', channel);
+    channel.onmessage = function (event) {
+        self.emit('message', channel.label, event.data, channel, event);
+    };
+    channel.onopen = this.emit.bind(this, 'channelOpen', channel);
+};
+
+// Fetch or create a data channel by the given name
+Peer.prototype.getDataChannel = function (name, opts) {
+    if (!webrtc.dataChannel) return this.emit('error', new Error('createDataChannel not supported'));
+    var channel = this.channels[name];
+    opts || (opts = {reliable: false});
+    if (channel) return channel;
+    // if we don't have one by this label, create it
+    channel = this.channels[name] = this.pc.pc.createDataChannel(name, opts);
+    this._observeDataChannel(channel);
+    return channel;
+};
+
 Peer.prototype.onIceCandidate = function (candidate) {
     if (this.closed) return;
     if (candidate) {
@@ -332,14 +373,22 @@ Peer.prototype.end = function () {
 };
 
 Peer.prototype.handleRemoteStreamAdded = function (event) {
-    this.stream = event.stream;
-    this.parent.emit('peerStreamAdded', this);
+    if (this.stream) {
+        this.logger.warn('Already have a remote stream');
+    } else {
+        this.stream = event.stream;
+        this.parent.emit('peerStreamAdded', this);
+    }
 };
 
 Peer.prototype.handleStreamRemoved = function () {
     this.parent.peers.splice(this.parent.peers.indexOf(this), 1);
     this.closed = true;
     this.parent.emit('peerStreamRemoved', this);
+};
+
+Peer.prototype.handleDataChannelAdded = function (channel) {
+    this.channels[channel.name] = channel;
 };
 
 module.exports = WebRTC;
