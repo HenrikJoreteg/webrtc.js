fcf032dcf3ca2bdfe1851ddcab66d3605be6b2c6
Philipp Hancke <fippo@andyet.net>
2014-05-22 14:08:52 +0200

    isInitiator seems broken

--- webrtc.js
+++ webrtc.js
@@ -176,7 +176,7 @@ function Peer(options) {
         case 'failed':
             // currently, in chrome only the initiator goes to failed
             // so we need to signal this to the peer
-            if (self.pc.config.isInitiator) {
+            if (self.pc.pc.peerconnection.localDescription.type === 'offer') {
                 self.parent.emit('iceFailed', self);
                 self.send('connectivityError');
             }
